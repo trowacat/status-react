b0bf0dda38bd2f268ee03576e33eca211f2803b8
Roman Volosovskyi <roman.wolosovskyi@gmail.com>
2019-02-12 19:03:34 +0200

    [wip] new bottom tab (react-navigation)

--- src/status_im/ui/screens/routing/chat_stack.cljs
+++ src/status_im/ui/screens/routing/chat_stack.cljs
@@ -1,46 +1,48 @@
 (ns status-im.ui.screens.routing.chat-stack
-  (:require [status-im.utils.config :as config]))
+  (:require [status-im.utils.config :as config]
+            [status-im.ui.components.react :as react]
+            [reagent.core :as reagent]))
 
 (def chat-stack
-  {:name    :chat-stack
-   :screens [{:name    :chat-main-stack
-              :screens (cond->
-                        [:home
-                         :chat
-                         :profile
-                         :new
-                         :new-chat
-                         :qr-scanner
-                         :take-picture
-                         :new-group
-                         :add-participants-toggle-list
-                         :contact-toggle-list
-                         :group-chat-profile
-                         :new-public-chat
-                         :open-dapp
-                         :dapp-description
-                         :browser
-                         :stickers
-                         :stickers-pack
-                         :login]
+  {:name       :chat-stack
+   :screens    [{:name    :chat-main-stack
+                 :screens (cond->
+                           [:home
+                            :chat
+                            :profile
+                            :new
+                            :new-chat
+                            :qr-scanner
+                            :take-picture
+                            :new-group
+                            :add-participants-toggle-list
+                            :contact-toggle-list
+                            :group-chat-profile
+                            :new-public-chat
+                            :open-dapp
+                            :dapp-description
+                            :browser
+                            :stickers
+                            :stickers-pack
+                            :login]
 
-                         config/hardwallet-enabled?
-                         (concat [:hardwallet-connect :enter-pin]))
-              :config  {:initialRouteName :home}}
-             :wallet-modal
-             :chat-modal
-             :show-extension-modal
-             {:name    :wallet-send-modal-stack
-              :screens [:wallet-send-transaction-modal
-                        :wallet-transaction-sent-modal
-                        :wallet-transaction-fee]
-              :config  {:initialRouteName :wallet-send-transaction-modal}}
-             {:name    :wallet-send-modal-stack-with-onboarding
-              :screens [:wallet-onboarding-setup-modal
-                        :wallet-send-transaction-modal
-                        :wallet-transaction-sent-modal
-                        :wallet-transaction-fee]
-              :config  {:initialRouteName :wallet-onboarding-setup-modal}}
-             :wallet-sign-message-modal]
-   :config  {:mode             :modal
-             :initialRouteName :chat-main-stack}})
+                            config/hardwallet-enabled?
+                            (concat [:hardwallet-connect :enter-pin]))
+                 :config  {:initialRouteName :home}}
+                :wallet-modal
+                :chat-modal
+                :show-extension-modal
+                {:name    :wallet-send-modal-stack
+                 :screens [:wallet-send-transaction-modal
+                           :wallet-transaction-sent-modal
+                           :wallet-transaction-fee]
+                 :config  {:initialRouteName :wallet-send-transaction-modal}}
+                {:name    :wallet-send-modal-stack-with-onboarding
+                 :screens [:wallet-onboarding-setup-modal
+                           :wallet-send-transaction-modal
+                           :wallet-transaction-sent-modal
+                           :wallet-transaction-fee]
+                 :config  {:initialRouteName :wallet-onboarding-setup-modal}}
+                :wallet-sign-message-modal]
+   :config     {:mode             :modal
+                :initialRouteName :chat-main-stack}})
